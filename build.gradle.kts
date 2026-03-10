09639af9a64c9abda61783ca7200acbc6aa85171
Pectics <pectics@outlook.com>
2026-03-11 06:12:51 +0800

    build: 全局添加 jetbrains-annotations 注解库依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ subprojects {
     }
 
     dependencies {
+        compileOnly("org.jetbrains:annotations:26.0.1")
         compileOnly("org.projectlombok:lombok:1.18.36")
         annotationProcessor("org.projectlombok:lombok:1.18.36")
 
