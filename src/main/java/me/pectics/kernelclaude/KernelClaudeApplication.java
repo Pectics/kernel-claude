25a446f769f497552cc6829518fff266b40fa7f8
Pectics <pectics@outlook.com>
2026-03-11 01:07:35 +0800

    chore: Initial commit

---
+++ src/main/java/me/pectics/kernelclaude/KernelClaudeApplication.java
@@ -0,0 +1,13 @@
+package me.pectics.kernelclaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KernelClaudeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(KernelClaudeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/pectics/kernelclaude/KernelClaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.pectics.kernelclaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KernelClaudeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
